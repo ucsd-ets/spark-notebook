56011ece4202b9b20bdff9d0b00014b1b66733fa
Thomaswang0822 <thomaswang0822@outlook.com>
2022-09-08 17:37:37 -0700

    change config; add cluster role to jupyter

--- jupyter_config.py
+++ jupyter_config.py
@@ -1,8 +1,6 @@
 c.ServerProxy.servers = {
   'test-server': {
-    'command': [
-        '/opt/spark-3.3.0-bin-hadoop3/sbin/start-master.sh',
-        '/opt/start-workers.sh'],
+    'command': ['/opt/spark-3.3.0-bin-hadoop3/bin/spark-class org.apache.spark.deploy.master.Master --host spark-jupyter --port 7077 --webui-port 8080 && /opt/start-workers.sh'],
     'absolute_url': False,
     'port': 8080
     }
--- start-workers.sh
+++ start-workers.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+# install helm chart
 helm install spark-notebook-chart /opt/spark-notebook-chart
