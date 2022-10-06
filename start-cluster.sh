4e14d360fc4778331133376d49689ff73d4bc540
Thomaswang0822 <thomaswang0822@outlook.com>
2022-10-06 16:49:40 -0700

    Correct env USER and edit helm install cmd

--- start-cluster.sh
+++ start-cluster.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 /opt/spark-3.3.0-bin-hadoop3/sbin/start-master.sh
 # install helm chart
-helm install spark-notebook-chart /opt/spark-notebook-chart --set uid=$(id -u)
+helm install spark-notebook-chart /opt/spark-notebook-chart --set uid=$(id -u) --set username=$USER
