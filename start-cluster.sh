a1e3b5ccef6bc0f0bcb343a0da13fd76752a5102
Thomaswang0822 <thomaswang0822@outlook.com>
2022-10-07 16:33:17 -0700

    change hostname in dashboard url

--- start-cluster.sh
+++ start-cluster.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 /opt/spark-3.3.0-bin-hadoop3/sbin/start-master.sh
 # install helm chart
-helm install spark-notebook-chart /opt/spark-notebook-chart --set uid=$(id -u) --set username=$USER
+helm install spark-notebook-chart /opt/spark-notebook-chart \
+--set masterHostName=$(hostname) \
+--set uid=$(id -u) \
+--set username=$USER
