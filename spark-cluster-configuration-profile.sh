d476f7b3c3a098056cdc30d60c79ff04e1300d2d
David Andersen <git@davidandersen.us>
2025-04-19 14:57:24 -0700

    disable logs

--- spark-cluster-configuration-profile.sh
+++ spark-cluster-configuration-profile.sh
@@ -13,14 +13,14 @@ SPARK_CLUSTER_REPLICAS=3
 [ -f ~/.spark-cluster-configuration-profile.sh ] && . ~/.spark-cluster-configuration-profile.sh
 
 MYUID=`id -u`
-echo --- >> ~/startup.log
-echo spark-cluster-configuration-profile.sh >>  ~/startup.log
-env >> ~/startup.log
-echo my uid is $MYUID
-id >> ~/startup.log
-# remove any user created with numeric uid
-echo before  >> ~/startup.log
-cat /tmp/passwd.wrap >> ~/startup.log
+# echo --- >> ~/startup.log
+# echo spark-cluster-configuration-profile.sh >>  ~/startup.log
+# env >> ~/startup.log
+# echo my uid is $MYUID
+# id >> ~/startup.log
+# # remove any user created with numeric uid
+# echo before  >> ~/startup.log
+# cat /tmp/passwd.wrap >> ~/startup.log
 sed -i "/^${MYUID}:/d" /tmp/passwd.wrap
-echo after >> ~/startup.log
-cat /tmp/passwd.wrap >> ~/startup.log
+# echo after >> ~/startup.log
+# cat /tmp/passwd.wrap >> ~/startup.log
