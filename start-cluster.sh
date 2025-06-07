452e715b008a52c7d0349e162474b7fd2ecec0f1
David Andersen <git@davidandersen.us>
2025-06-07 10:15:00 -0700

    move dataset to fs04

--- start-cluster.sh
+++ start-cluster.sh
@@ -34,7 +34,7 @@ helm install $SPARK_CHART_NAME /opt/spark \
     --set worker.memoryLimit=${SPARK_CLUSTER_WORKER_APP_MEM:-18G} \
     --set-json="worker.extraVolumes[0]={\"name\":\"course-workspace\",\"nfs\":{\"server\":\"${FILESYSTEM}\",\"path\":\"${WORKSPACE}\"}}" \
     --set-json='worker.extraVolumes[1]={"name":"home","persistentVolumeClaim":{"claimName":"home"}}' \
-    --set-json="worker.extraVolumes[2]={\"name\":\"datasets\",\"nfs\":{\"server\":\"its-dsmlp-fs03.ucsd.edu\",\"path\":\"/export/datasets\"}}" \
+    --set-json="worker.extraVolumes[2]={\"name\":\"datasets\",\"nfs\":{\"server\":\"its-dsmlp-fs04.ucsd.edu\",\"path\":\"/export/workspaces/DSC102_SP25_A00/public/dataset_public\"}}" \
     --set-json='worker.extraVolumeMounts[0]={"name":"course-workspace","mountPath":"/home/${USER}"}' \
     --set worker.extraVolumeMounts[0].mountPath=/home/$USER \
     --set worker.extraVolumeMounts[0].subPath=home/$USER \
@@ -43,10 +43,10 @@ helm install $SPARK_CHART_NAME /opt/spark \
     --set worker.extraVolumeMounts[1].subPath=public \
     --set-json='worker.extraVolumeMounts[2]={"name":"home","mountPath":"/home/${USER}/private"}' \
     --set worker.extraVolumeMounts[2].mountPath=/home/$USER/private \
-    --set-json='worker.extraVolumeMounts[3]={"name":"datasets","mountPath":"/datasets"}' \
+    --set-json='worker.extraVolumeMounts[3]={"name":"datasets","mountPath":"/datasets/courses/dsc102/public"}' \
     --set-json="master.extraVolumes[0]={\"name\":\"course-workspace\",\"nfs\":{\"server\":\"${FILESYSTEM}\",\"path\":\"${WORKSPACE}\"}}" \
     --set-json='master.extraVolumes[1]={"name":"home","persistentVolumeClaim":{"claimName":"home"}}' \
-    --set-json="master.extraVolumes[2]={\"name\":\"datasets\",\"nfs\":{\"server\":\"its-dsmlp-fs03.ucsd.edu\",\"path\":\"/export/datasets\"}}" \
+    --set-json="master.extraVolumes[2]={\"name\":\"datasets\",\"nfs\":{\"server\":\"its-dsmlp-fs04.ucsd.edu\",\"path\":\"/export/workspaces/DSC102_SP25_A00/public/dataset_public\"}}" \
     --set-json='master.extraVolumeMounts[0]={"name":"course-workspace","mountPath":"/home/${USER}"}' \
     --set master.extraVolumeMounts[0].mountPath=/home/$USER \
     --set master.extraVolumeMounts[0].subPath=home/$USER \
@@ -54,5 +54,5 @@ helm install $SPARK_CHART_NAME /opt/spark \
     --set master.extraVolumeMounts[1].mountPath=/home/$USER/public \
     --set master.extraVolumeMounts[1].subPath=public \
     --set-json='master.extraVolumeMounts[2]={"name":"home","mountPath":"/home/${USER}/private"}' \
-    --set-json='master.extraVolumeMounts[3]={"name":"datasets","mountPath":"/datasets"}' \
+    --set-json='master.extraVolumeMounts[3]={"name":"datasets","mountPath":"/datasets/courses/dsc102/public"}' \
     --set master.extraVolumeMounts[2].mountPath=/home/$USER/private
