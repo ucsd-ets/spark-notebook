c1d59417b2ddc5776c76b32b08f14e291b4b9d35
N2akiti <n2akiti@ucsd.edu>
2025-08-04 15:17:35 -0400

    Update start-cluster.sh

--- start-cluster.sh
+++ start-cluster.sh
@@ -13,6 +13,8 @@ helm install $SPARK_CHART_NAME /opt/spark \
     --set global.security.allowInsecureImages=true \
     --set serviceAccount.name=default \
     --set serviceAccount.create=false \
+    --set master.pdb.create=false \
+    --set worker.pdb.create=false \
     --set master.networkPolicy.enabled=false \
     --set worker.networkPolicy.enabled=false \
     --set master.podSecurityContext.runAsUser=$UID \
