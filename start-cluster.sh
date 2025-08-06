f1b44d2c6a5fd57f665fdd78e1bbff6066b5b8e8
N2akiti <n2akiti@ucsd.edu>
2025-08-06 17:48:43 -0400

    Update start-cluster.sh

--- start-cluster.sh
+++ start-cluster.sh
@@ -6,9 +6,9 @@ WORKSPACE=$(dirname $HOMEMOUNT)
 WORKSPACE=$(dirname $WORKSPACE)
 
 helm install $SPARK_CHART_NAME /opt/spark \
-    --set image.registry=ghcr.io \
-    --set image.repository=ucsd-ets/spark-node \
-    --set image.tag=feature-submodule-15 \
+    --set image.registry=${SPARK_CLUSTER_IMAGE_REGISTRY:-ghcr.io} \
+    --set image.repository=${SPARK_CLUSTER_IMAGE_REPO:-ucsd-ets/spark-node} \
+    --set image.tag=${SPARK_CLUSTER_IMAGE_TAG:-feature-submodule-15} \
     --set image.pullPolicy=Always \
     --set global.security.allowInsecureImages=true \
     --set serviceAccount.name=default \
